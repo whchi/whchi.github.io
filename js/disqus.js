36fe10c6e4bb2a510699b5b768affcb0564a8d7d
DylanChi <dylanchi@cw.com.tw>
2018-12-20 17:40:56 +0800

    sync

--- js/disqus.js
+++
@@ -1,8 +0,0 @@
-var disqus_shortname = document.getElementById('disqus-shortname').getAttribute('data-disqus-shortname');
-(function() {
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
